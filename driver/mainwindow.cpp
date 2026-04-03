abaa9a126429f884c857d75dbc08a152d60fef9f
virtualgrub39 <virtualgrub39@tutamail.com>
2026-04-03 17:22:28 +0200

    driver: added basic state-machine

--- driver/mainwindow.cpp
+++ driver/mainwindow.cpp
@@ -2,10 +2,14 @@
 
 #include <QSerialPortInfo>
 #include <QtWidgets>
+#include <qhashfunctions.h>
+#include <qserialport.h>
 
 MainWindow::MainWindow (QWidget *parent) : QMainWindow (parent)
 {
     ui.setupUi (this);
+
+    stage = Stage::Disconnected;
 
     for (auto &info : QSerialPortInfo::availablePorts ()) ui.portSelector->addItem (info.portName ());
 
@@ -14,10 +18,67 @@ MainWindow::MainWindow (QWidget *parent) : QMainWindow (parent)
     connect (ui.writeButton, &QPushButton::clicked, this, &MainWindow::onWriteButtonClick);
 }
 
+void MainWindow::configurePort(qint32 baud)
+{
+    port.setBaudRate(baud);
+    port.setDataBits(QSerialPort::Data8);
+    port.setParity(QSerialPort::NoParity);
+    port.setStopBits(QSerialPort::OneStop);
+    port.setFlowControl(QSerialPort::NoFlowControl);
+}
+
+void
+MainWindow::portConnect ()
+{
+    auto port_name = ui.portSelector->currentText();
+    auto baud_txt = ui.baudInput->text();
+    if (baud_txt.length() == 0)
+    {
+        ui.statusBar->showMessage("Invalid baud rate");
+        return;
+    }
+
+    port.close();
+    stage = Stage::Disconnected;
+
+    port.setPortName(port_name);
+    if (!port.open(QIODevice::ReadWrite)) {
+        ui.statusBar->showMessage(tr("Can't open %1, error code %2")
+                   .arg(port_name).arg(port.error()), 0);
+        return;
+    }
+    configurePort(baud_txt.toUInt());
+
+    connect(&port, &QSerialPort::readyRead,
+            this,  &MainWindow::onReadyRead);
+
+    rxBuf.clear();
+    sendReset();
+
+    ui.statusBar->showMessage(tr("Connecting to %1…").arg(port_name));
+}
+
+void
+MainWindow::portDisconnect ()
+{
+    port.close();
+    stage = Stage::Disconnected;
+    ui.statusBar->showMessage(tr("Disconnected"), 0);
+    ui.connectButton->setText("CONNECT");
+}
+
 void
 MainWindow::onConnectButtonClick ()
 {
-    ui.statusBar->showMessage ("CONNECT CLICKED!", 2000);
+    if (port.isOpen())
+        portDisconnect();
+    else portConnect();
+}
+
+void MainWindow::onReadyRead()
+{
+    rxBuf += port.readAll();
+    processIncoming();
 }
 
 void
@@ -31,3 +92,62 @@ MainWindow::onWriteButtonClick ()
 {
     ui.statusBar->showMessage ("WRITE CLICKED!", 2000);
 }
+
+void MainWindow::sendReset()
+{
+    port.write("\x03");   
+}
+
+void MainWindow::processIncoming()
+{
+    while (rxBuf.contains("\r\n"))
+    {
+        int end  = rxBuf.indexOf("\r\n");
+        QByteArray line = rxBuf.left(end).trimmed();
+        rxBuf.remove(0, end + 2);
+
+        if (line.length() == 0) continue;
+        if (line == "RESET")
+        {
+            stage = Stage::Disconnected;
+            ui.statusBar->showMessage(tr("Device Reset"), 0);
+            continue;
+        }
+
+        switch (stage)
+        {
+        case Stage::Disconnected: {
+            if (line == "READY") {
+                stage = Stage::Connected;
+                ui.statusBar->showMessage(tr("Connected"), 0);
+                ui.connectButton->setText("DISCONNECT");
+            }
+
+            break;
+        }
+        default: qDebug() << "Device:" << line;
+        }
+    }
+}
+
+// template <std::unsigned_integral T>
+// void MainWindow::ihexRead(T& out)
+// {
+    
+// }
+
+// template <std::unsigned_integral T>
+// void MainWindow::ihexWrite(T value)
+// {
+
+// }
+
+// void MainWindow::ihexRead(span<uint8_t> out)
+// {
+
+// }
+
+// void MainWindow::ihexWrite(span<const uint8_t> data)
+// {
+    
+// }
--- driver/mainwindow.h
+++ driver/mainwindow.h
@@ -1,7 +1,15 @@
 #pragma once
 
 #include "ui_mainwindow.h"
+
+#include <span>
+#include <cstdint>
+#include <concepts>
+
 #include <QMainWindow>
+#include <QSerialPort>
+
+using std::span;
 
 class MainWindow : public QMainWindow
 {
@@ -16,5 +24,30 @@ class MainWindow : public QMainWindow
     void onWriteButtonClick ();
 
   private:
+    enum class Stage {
+        Disconnected,
+        Connected,
+        Reading,
+        Writing,
+    } stage;
+
     Ui::MainWindow ui;
+    QSerialPort port;
+    QByteArray  rxBuf;
+
+    void portConnect();
+    void portDisconnect();
+    void configurePort(qint32 baud);
+    void onReadyRead();
+    void sendReset();
+    void processIncoming();
+
+    // template <std::unsigned_integral T>
+    // void ihexRead(T& out);
+
+    // template <std::unsigned_integral T>
+    // void ihexWrite(T value);
+
+    // void ihexRead(span<uint8_t> out);
+    // void ihexWrite(span<const uint8_t> data);
 };
